b2d7bb80f79caae1bde91e5281336070dbdaec6d
tbright7 <travisbright7@gmail.com>
2025-04-06 18:05:13 -0700

    removed console.log

--- app/components/rentals/filter.js
+++ app/components/rentals/filter.js
@@ -3,7 +3,6 @@ import Component from '@glimmer/component';
 export default class RentalsFilter extends Component {
   get results() {
     let { rentals, query } = this.args;
-    console.log(rentals);
     if (query) {
       rentals = rentals.filter((rental) =>
         rental.title.toLowerCase().includes(query.toLowerCase()),
